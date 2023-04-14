8dc171fc5ea9472b540cf769a1b44289cd61f14d
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-04-15 00:44:45 +0400

    data for starting the server is taken from the environment file

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 4200;
+  const port = process.env.SERVER_PORT || 4200;
   const app = await NestFactory.create(AppModule);
   await app.listen(port, () => console.log(`Server started on port = ${port}`));
 }
