54f5f71aac0aa285c287b1c981545d6a5e2b02f8
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-04-15 00:38:55 +0400

    remove unnecessary

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 4200;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => console.log(`Server started on port = ${port}`));
 }
 bootstrap();
