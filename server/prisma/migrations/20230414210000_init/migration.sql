6df55a05c333abbd4226e8a8648cb3b543a79d5e
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-04-15 01:00:47 +0400

    import prisma orm

---
+++ server/prisma/migrations/20230414210000_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_phone_number_key" ON "user"("phone_number");
