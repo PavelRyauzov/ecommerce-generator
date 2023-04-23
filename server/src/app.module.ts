0ad0113ba6fd7581dd33612028f97405b1b47bc2
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-04-23 16:34:02 +0300

    import and configured prisma orm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true, envFilePath: '../.env'})],
+  imports: [ConfigModule.forRoot({isGlobal: true, envFilePath: '../.env'}), UserModule],
   controllers: [],
   providers: [],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService);
   const port = config.get<number>('API_PORT');
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(port || 4200, () => {
     console.log(`Server started on port: ${port}`);
   });
