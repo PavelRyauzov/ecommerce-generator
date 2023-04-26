168aa2434cc0ff4aa9a1afe8c3dbb4e230a11ab9
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-04-26 23:49:53 +0300

    added serving static files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CollectionModule } from './collection/collection.module';
 import { ProductModule } from './product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public/static'),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
